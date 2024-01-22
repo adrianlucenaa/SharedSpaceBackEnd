2e95daf552018db5547dcdce52259f891dae8762
Sergio Cubero <scubcar521@gmail.es>
2024-01-22 09:38:05 +0100

    class user

--- src/main/java/org/example/Models/User.java
+++ src/main/java/org/example/Models/User.java
@@ -1,4 +1,100 @@
 package org.example.Models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name",length=256 ,nullable = false)
+    private String name;
+
+    @Column(name = "surname",length=256 ,nullable = false)
+    private String surname;
+
+    @Column(name = "email",length=256 ,nullable = false)
+    private String email;
+
+    @Column(name = "password",length=256 ,nullable = false)
+    private String password;
+
+    @Column(name = "dni",length=256 ,nullable = false)
+    private String dni;
+
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String surname, String email, String password, String dni) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.dni = dni;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+        public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", dni=" + dni +
+                '}';
+    }
 }
+
