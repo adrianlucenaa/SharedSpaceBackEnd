eb58b8dd0aa302c120b0a0e0c6a8af65a1c59a18
Adrian <alucvar0812@g.educaand.es>
2024-02-28 19:39:24 +0100

    Implantado de CRS de task y Controller echo, sin test

---
+++ src/main/java/org/example/Controllers/TaskController.java
@@ -0,0 +1,57 @@
+package org.example.Controllers;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    TaskService service;
+
+    //Devuelve todas las tareas
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks(){
+        List<Task> tasks = service.getAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    //Devuelve todas las tareas por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable("id") int id){
+        Task task = service.getTaskById(id);
+        return ResponseEntity.ok(task);
+    }
+
+    //Crea o actualiza una tarea
+    @PostMapping
+    public ResponseEntity<Task> createOrUpdateTask(@RequestBody Task task){
+        Task end = service.createOrUpdateTask(task);
+        return ResponseEntity.ok(end);
+    }
+
+    //Elimina una tarea
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable("id") int id){
+        service.deleteTask(id);
+    }
+
+    //Devuelve todas las tareas de un apartamento
+    @GetMapping("/apartment/{id}")
+    public ResponseEntity<List<Task>> getTasksByApartmentId(@PathVariable("id") int id){
+        List<Task> tasks = service.getTasksByApartmentId(id);
+        return ResponseEntity.ok(tasks);
+    }
+
+    //Devuelve todas las tareas de un usuario
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<Task>> getTasksByUserId(@PathVariable("id") int id){
+        List<Task> tasks = service.getTasksByUserId(id);
+        return ResponseEntity.ok(tasks);
+    }
+
+    //Devuelve todas las tareas de un apartamento y un usuario
+    @GetMapping("/apartment/{id}/user/{id2}")
+    public ResponseEntity<List<Task>> getTasksByApartmentIdAndUserId(@PathVariable("id") int id, @PathVariable("id2") int id2){
+        List<Task> tasks = service.getTasksByApartmentIdAndUserId(id, id2);
+        return ResponseEntity.ok(tasks);
+    }
+}
---
+++ src/main/java/org/example/repository/TaskRepository.java
@@ -0,0 +1,4 @@
+package org.example.repository;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
